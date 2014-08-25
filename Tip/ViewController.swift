aad16313d635dc7ef8f79a07e6d1c81f4b99fbf7
loheiman <Loren@LHMBP.local>
2014-08-24 23:25:55 -0700

    Ta Da
    @thecodepath and @timothy1ee.
    
    Because I personally like to have the charges on my credit card from
    restaurants be whole numbers, i added a rounding toggle that does so.

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var roundSwitch: UISwitch!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text="0.00"
+        totalLabel.text="0.00"
+        roundSwitch.on = false
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = tip + billAmount
+        
+    
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
 
+        
+        if roundSwitch.on == true {
+            total = round(total)
+            tip = total - billAmount
+            println("rounded!")
+        }
+      
+        
+        tipLabel.text = String(format: "$%.2f" , tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
