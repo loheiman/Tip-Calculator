8f5b2a49e355c8b06304db74801c909f1a6577b3
loheiman <Loren@LHMBP.local>
2014-08-30 13:55:56 -0700

    Making it Pretty continued

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -8,26 +8,52 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
     
     
+    
+
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var roundSwitch: UISwitch!
     
-
-                            
+    @IBOutlet weak var billTextLabel: UILabel!
+    @IBOutlet weak var tipPercentageTextLabel: UILabel!
+    @IBOutlet weak var tipTextLabel: UILabel!
+    @IBOutlet weak var totalTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
      //   tipLabel.text="0.00"
        // totalLabel.text="0.00"
-      //  roundSwitch.on = false
+        // Optionally initialize the property to a desired starting value
+        
+        self.billField.alpha = 0
+        self.billTextLabel.alpha = 0
+        
+        self.tipControl.alpha = 0
+        self.tipLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        self.tipPercentageTextLabel.alpha = 0
+        self.tipTextLabel.alpha = 0
+        self.totalTextLabel.alpha = 0
         
         billField.becomeFirstResponder()
-     
+        
+
+
+        UIView.animateWithDuration(0.5, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.billField.alpha = 1
+            self.billTextLabel.alpha = 1
+            })
+        
+        
     }
 
     
@@ -36,38 +62,67 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    
+    
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        
-        var tipPercentages = [0.18, 0.2, 0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        
-        
-        var billAmount = NSString(string: billField.text).doubleValue
     
+        var tipPercentages = [0.15, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+       
+        var billAmount = NSString(string: billField.text).doubleValue
         var tip = billAmount * tipPercentage
         var total = tip + billAmount
+        
+        total = round(total)
+        tip = total - billAmount
+    
         
     
         tipLabel.text = "\(tip)"
         totalLabel.text = "\(total)"
         
-
-        
-     //   if roundSwitch.on == true {
-            total = round(total)
-            tip = total - billAmount
-          //  println("rounded!")
-      //  }
-      
-        
         tipLabel.text = String(format: "%.2f" , tip)
         totalLabel.text = String(format: "%.2f", total)
+        
+        self.tipLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        
+        
+        UIView.animateWithDuration(0.4, animations: {
+            self.tipControl.alpha = 1
+            self.tipPercentageTextLabel.alpha = 1
+            self.tipTextLabel.alpha = 1
+            self.totalTextLabel.alpha = 1
+            self.tipLabel.alpha = 1
+            self.totalLabel.alpha = 1
+            
+            
+        })
+
     }
 
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+    @IBAction func tipChanged(sender: AnyObject) {
+        
+        self.tipLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        
+       UIView.animateWithDuration(0.4, animations: {
+        self.tipLabel.alpha = 1
+        self.totalLabel.alpha = 1
+        })
     }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+      view.endEditing(true)
+    }
+    
+    
+    
 }
+
+
 
